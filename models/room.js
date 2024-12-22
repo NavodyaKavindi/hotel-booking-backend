3b25c519928d9fb90ae08580c3a4d5fd4edba53c
NavodyaKavindi <131426014+NavodyaKavindi@users.noreply.github.com>
2024-12-22 21:56:49 +0530

    create a room module

--- models/room.js
+++ models/room.js
@@ -1,5 +1,51 @@
 import mongoose from "mongoose" ; 
 const roomSchema= new mongoose.Schema(
+{
+rooId:{
+type: Number,
+required: true,
+unque: true
 
-    
+},
+
+category:{
+type: String,
+required: true
+
+},
+
+maxGuests: {
+    type: Number,
+    required: true,
+    default: 3,
+},
+
+available:{
+    type: Boolean,
+    required: true,
+    default: true
+},
+
+photos: [
+    {
+        type: String
+    }
+],
+
+specialDescription: {
+    type: String,
+    default: ""
+},
+
+notes: {
+    type: String,
+    default:""
+}
+
+}
+
 )
+
+const Room = mongoose.model("Rooms", roomSchema)
+
+export default Room;
