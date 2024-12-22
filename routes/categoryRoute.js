217929332593033760aaf2dbb535a1f1441685b0
NavodyaKavindi <131426014+NavodyaKavindi@users.noreply.github.com>
2024-12-22 21:40:55 +0530

    create update in category

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -3,13 +3,14 @@ import{createCategory} from '../controllers/categoryController.js'
 import { deleteCategory } from '../controllers/categoryController.js';
 import { getCategory } from '../controllers/categoryController.js';
 import { getCategoryByName } from '../controllers/categoryController.js';
+import { updateCategory } from '../controllers/categoryController.js';
 
  const categoryRouter= express.Router()
 
 
  categoryRouter.post("/",createCategory)
 
- categoryRouter.delete("/:name",deleteCategory)
+ categoryRouter.delete("/:name",deleteCategory) 
  
 
  categoryRouter.get("/searchByPrice", (req,res)=>{
@@ -19,5 +20,7 @@ import { getCategoryByName } from '../controllers/categoryController.js';
  })
  categoryRouter.get("/:name",getCategoryByName)
  categoryRouter.get("/",getCategory);
+
+ categoryRouter.put("/:name",updateCategory)
 
  export default categoryRouter;
